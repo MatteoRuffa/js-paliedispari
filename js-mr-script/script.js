69d22f37cc6f303c95eda5bf384726728afff64f
MatteoRuffa <cortese_17@protonmail.com>
2024-03-14 16:11:06 +0100

    reactivated 1^es

--- js-mr-script/script.js
+++ js-mr-script/script.js
@@ -4,41 +4,41 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// const word = prompt('inserisci una parola')
-// console.log(word);
+const word = prompt('inserisci una parola')
+console.log(word);
 
 // prova di spilt!
 // let parola = "prova";
 // let lettere = parola.split('');
 // console.log(lettere);
 
-// let result = false;
+let result = false;
 
 /* creo una funzione che riconosce le lettere in una parola e 
 analizza se sono posizionate nel modo giusto per formare una parola palindroma */
-// function wordAnalysis() {
-//     let letter = word.split('');
-//     console.log(letter);     //chiedere ai prof perche viene stampato due volte(forse perche esegue la funz. due volte?)
-//     // fare un ciclo per analizzare le lettere e vedere se sono uguali alla rispettiva posizione
-//     let n = 0; 
-//     while (n < letter.length / 2) {
-//         if (letter[n] !== letter[letter.length - (n + 1)]) {
-//         return result = false;
-//         }
-//         n++;
-//     }
-//     return result = true;
-// }
-
-// result = wordAnalysis()
-// console.log(wordAnalysis());
+function wordAnalysis() {
+    let letter = word.split('');
+    console.log(letter);     //chiedere ai prof perche viene stampato due volte(forse perche esegue la funz. due volte?)
+    // fare un ciclo per analizzare le lettere e vedere se sono uguali alla rispettiva posizione
+    let n = 0; 
+    while (n < letter.length / 2) {
+        if (letter[n] !== letter[letter.length - (n + 1)]) {
+            return result = false;
+        }
+        n++;
+    }
+    return result = true;
+}
+console.log(wordAnalysis());
 
 // aggiungiamo messaggio stampa 
-// if (result) {
-//     console.log("La parola è un palindromo");
-// } else {
-//     console.log("La parola non è un palindromo");
-// }
+if (result) {
+    console.log("La parola è un palindromo");
+} else {
+    console.log("La parola non è un palindromo");
+}
+
+
 
 /*
 Pari e Dispari

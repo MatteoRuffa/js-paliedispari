b06fde6ecbe12350954ed527ef366ac876c0e99d
MatteoRuffa <cortese_17@protonmail.com>
2024-03-14 14:26:58 +0100

    add intro e logic

---
+++ js-mr-script/script.js
@@ -0,0 +1,19 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+const word = prompt('inserisci una parola')
+console.log(word);
+
+// prova di spilt!
+// let parola = "prova";
+// let lettere = parola.split('');
+// console.log(lettere);
+
+/* creo una funzione che riconosce le lettere in una parola e 
+analizza se sono posizionate nel modo giusto per formare una parola palindroma */
+function analisiParola() {
+    
+}
